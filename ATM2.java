f0a21690264114aef5639530afd0690a658f6091
Prajin  Patil <113690991+prajinpatil42@users.noreply.github.com>
2024-01-06 22:28:14 +0530

    Description
     create a mini-application for a banking system in Java. In this program, we will add some basic functionalities of a bank account like a deposit of amount, withdrawal of amount, etc.

---
+++ ATM2.java
@@ -0,0 +1,44 @@
+package oops;
+
+
+
+public class ATM2 {
+	
+	private double balance;
+	private double depositamount;
+	private double withdrawamount;
+	
+	//default constructor
+ public ATM2() {
+		
+	}
+
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public double getDepositamount() {
+		return depositamount;
+	}
+
+	public void setDepositamount(double depositamount) {
+		this.depositamount = depositamount;
+	}
+
+	public double getWithdrawamount() {
+		return withdrawamount;
+	}
+
+	public void setWithdrawamount(double withdrawamount) {
+		this.withdrawamount = withdrawamount;
+	}
+	
+	//getter,setter
+	
+
+}
